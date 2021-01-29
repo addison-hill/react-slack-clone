443d8a22965619b27863aadc54c8474c04a64471
Addison Hill <adddison.hill@gmail.com>
2021-01-28 19:36:27 -0600

    added redux for global state

--- src/index.js
+++ src/index.js
@@ -10,11 +10,20 @@ import 'semantic-ui-css/semantic.min.css'
 
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
 
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools())
+
 class Root extends React.Component {
 
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
+                this.props.setUser(user);
                 this.props.history.push('/');
             }
         })
@@ -34,12 +43,14 @@ class Root extends React.Component {
     }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-    <Router>
-        <RootWithAuth /> 
-    </Router>, 
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth /> 
+        </Router>
+    </Provider>, 
     document.getElementById('root')
 );
 registerServiceWorker();
