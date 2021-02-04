5e4a1331655ede4622ff04169b4af6bb7b02a9a5
Addison Hill <adddison.hill@gmail.com>
2021-02-04 14:36:49 -0600

    added avatar to user panel

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import firebase from '../../firebase';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react'
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react'
 
 class UserPanel extends React.Component {
     state = {
@@ -31,6 +31,8 @@ class UserPanel extends React.Component {
     }
 
     render() {
+        const { user } = this.state;
+
         return (
             <Grid style={{ background: '#4c3c4c' }}>
                 <Grid.Column>
@@ -40,14 +42,18 @@ class UserPanel extends React.Component {
                             <Icon name="code" />
                             <Header.Content>DevChat</Header.Content>
                         </Header>
+                        
+                        {/* User Dropdown */}
+                        <Header style={{ padding: '0.25em' }} as="h4" inverted>
+                            <Dropdown trigger={
+                                <span>
+                                    <Image src={user.photoUrl} spaced="right" avatar />
+                                    {user.displayName}
+                                </span>
+                            } options={this.dropdownOptions()} />
+                        </Header>
                     </Grid.Row>
 
-                    {/* User Dropdown */}
-                    <Header style={{ padding: '0.25em' }} as="h4" inverted>
-                        <Dropdown trigger={
-                            <span>{this.state.user.displayName}</span>
-                        } options={this.dropdownOptions()} />
-                    </Header>
                 </Grid.Column>
             </Grid>
 
